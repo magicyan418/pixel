d791ebd796c67ed8937e93e05c13ddf04c72df2d
yanqinlan <2314741287@qq.com>
2025-04-15 16:49:13 +0800

    feat: pixel home

---
+++ src/app/monopoly/page.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: 严钦蓝
+ * @FilePath: \pixel-nextjs\src\app\Monopoly\page.tsx
+ * @LastEditors: 严钦蓝
+ * @LastEditTime: 2025-04-15 14:44:59
+ * @Description: 
+ */
+const Monopoly = () => {
+  return <div>Monopoly</div>;
+};
+
+export default Monopoly;
+
